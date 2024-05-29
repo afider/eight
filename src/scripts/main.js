976ba81902520352ec28822a341b5e11020fa185
afider <e.beskrovnyy@gmail.com>
2024-05-29 21:28:26 +0300

    add countdown

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,7 @@ import toggleTabs from '@js/modules/toggle-tabs';
 import setNavActiveState from '@js/modules/nav-active-state';
 import getWalletData from '@js/modules/get-wallet-data';
 import validateInput from '@js/modules/validate-input';
+import setCountdown from '@js/modules/countdown';
 
 onload = () => {
   document.documentElement.style.scrollBehavior = "smooth";
@@ -13,3 +14,4 @@ toggleTabs();
 setNavActiveState();
 getWalletData();
 validateInput();
+setCountdown();
---
+++ src/scripts/modules/countdown.js
@@ -0,0 +1,30 @@
+export default function setCountdown() {
+
+  let timeControl = document.querySelector('#timeControl');
+  if (!timeControl) return
+  let second = 1000;
+  let minute = second * 60;
+  let hour = minute * 60;
+  let day = hour * 24;
+  
+  let utcTime = new Date('May 30 2024 20:08:00 UTC');
+  let countDown = utcTime.getTime();
+  
+  const doCountdown = () => {
+  
+      let nowDate = new Date().getTime();
+      let distance = countDown - nowDate;
+      
+      document.getElementById('days').innerText = Math.floor(distance / (day));
+      document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour));
+      document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute));
+      document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
+
+      if (distance < 0) {
+          clearInterval(countdownTimer);
+          timeControl.innerHTML = 'Wake Up'
+      }
+  }
+  
+  countdownTimer = setInterval(doCountdown, 1000);
+}
