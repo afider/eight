828c8a7c7a7ac9a7f759925aa7ed8e2e0729d09d
afider <e.beskrovnyy@gmail.com>
2024-05-30 22:57:24 +0300

    fix for see my box

--- src/scripts/modules/countdown.js
+++ src/scripts/modules/countdown.js
@@ -22,7 +22,7 @@ export default function setCountdown() {
 
       if (distance < 0) {
           clearInterval(countdownTimer);
-          timeControl.innerHTML = '<a href="/box" class="btn btn--wide">See your box</a>'
+          timeControl.innerHTML = '<a href="/box" class="btn btn--wide">See my box</a>'
       }
   }
   
